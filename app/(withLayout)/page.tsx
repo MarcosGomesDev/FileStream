a0cf015ebb22e66cecd332f43f76f9fe89ddbc40
Marcos Gomes <marcosgomesdev@gmail.com>
2024-06-25 17:48:31 -0300

    feat: add withLayout pages

---
+++ app/(withLayout)/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <div className="container">
+        <h1>FileStream</h1>
+
+        <div className="h-[800px]"></div>
+      </div>
+    </>
+  );
+}
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="container">
-      <h1>FileStream</h1>
-    </div>
-  );
-}
