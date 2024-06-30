8988b6a003a2c4fc2f2508130da02032389f71bc
Marcos Gomes <marcosgomesdev@gmail.com>
2024-06-30 02:51:47 -0300

    style: change layout size on mobile and desk for FileCard component

--- app/(withLayout)/folders/[...id]/components/FileCard/index.tsx
+++ app/(withLayout)/folders/[...id]/components/FileCard/index.tsx
@@ -15,20 +15,22 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { File } from "lucide-react";
+import { EllipsisVerticalIcon, File } from "lucide-react";
+import { HTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
 
-interface FileCardProps {
+interface FileCardProps extends HTMLAttributes<HTMLDivElement> {
   item: Folder;
 }
 
-export function FileCard({ item }: FileCardProps) {
+export function FileCard({ item, className }: FileCardProps) {
   return (
-    <Card className="h-36 min-w-[140px] max-w-[140px]">
+    <Card className={twMerge(["h-44 min-w-[140px] max-w-[140px]", className])}>
       <CardContent className="flex w-full flex-col p-0">
-        <div className="flex h-16 w-full items-center justify-center rounded-tl-md rounded-tr-md bg-primary">
+        <div className="flex h-24 w-full items-center justify-center rounded-tl-md rounded-tr-md bg-primary">
           <File size={36} className="text-white" />
         </div>
-        <div className="flex items-center px-1 py-2">
+        <div className="flex items-center justify-between px-1 py-1">
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild className="overflow-hidden">
@@ -39,39 +41,39 @@ export function FileCard({ item }: FileCardProps) {
               </TooltipContent>
             </Tooltip>
           </TooltipProvider>
+          <Popover>
+            <PopoverTrigger asChild className="right-2 flex self-end">
+              <Button variant="ghost" size="icon">
+                <EllipsisVerticalIcon size={16} />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-40" side="right">
+              <div className="flex flex-col">
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  onClick={() => {
+                    navigator.clipboard.writeText(`${item.path}`);
+                  }}
+                >
+                  Copiar link
+                </Button>
+
+                <Separator className="my-1" />
+
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  onClick={() => {
+                    navigator.clipboard.writeText(`${item.path}`);
+                  }}
+                >
+                  Fazer download
+                </Button>
+              </div>
+            </PopoverContent>
+          </Popover>
         </div>
-        <Popover>
-          <PopoverTrigger asChild className="right-2 flex self-end">
-            <Button variant="ghost" size="icon">
-              ...
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-40" side="right">
-            <div className="flex flex-col">
-              <Button
-                variant="ghost"
-                size="sm"
-                onClick={() => {
-                  navigator.clipboard.writeText(`${item.path}`);
-                }}
-              >
-                Copiar link
-              </Button>
-
-              <Separator className="my-5" />
-
-              <Button
-                variant="ghost"
-                size="sm"
-                onClick={() => {
-                  navigator.clipboard.writeText(`${item.path}`);
-                }}
-              >
-                Fazer download
-              </Button>
-            </div>
-          </PopoverContent>
-        </Popover>
       </CardContent>
     </Card>
   );
